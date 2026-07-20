021bff11191fef3386493d315e7bc1824002f8bb
Gytis Kupčinskas <gytiskupcinskas0@gmail.com>
2026-07-20 12:43:36 +0200

    new GetAll methods

--- Runtime/GameData.cs
+++ Runtime/GameData.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Gytis0.SOManager.Runtime
@@ -55,6 +56,53 @@ namespace Gytis0.SOManager.Runtime
 		}
 
 		/// <summary>
+		/// Gets all items of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <remarks>
+		/// Gets the original assets from the SOManager. Do not modify the properties.
+		/// </remarks>
+		/// <typeparam name="T">Type of the item.</typeparam>
+		/// <returns>All items, if any found. Else, an empty list.</returns>
+		/// <exception cref="Exception"></exception>
+		public static IEnumerable<T> GetAll<T>() where T : GameDataSO
+		{
+			if (!Initialized)
+				throw new Exception(string.Format("GameData not initialized. You can use a '{0}' flag and / or '{1}' event to wait for initialization.", nameof(Initialized), nameof(OnInitialize)));
+
+			if (!_data.TryGetValue(typeof(T), out var map))
+				yield break;
+
+			foreach (GameDataSO value in map.Values)
+				yield return (T)value;
+		}
+
+		/// <summary>
+		/// Gets copies of all items of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <remarks>
+		/// Gets copies of the assets from the SOManager. You may modify the properties.
+		/// </remarks>
+		/// <typeparam name="T">Type of the items.</typeparam>
+		/// <returns>A list containing copies of all items.</returns>
+		/// <exception cref="Exception"></exception>
+		public static List<T> GetAllCopy<T>()
+			where T : GameDataSO
+		{
+			if (!Initialized)
+				throw new Exception(string.Format("GameData not initialized. You can use a '{0}' flag and / or '{1}' event to wait for initialization.", nameof(Initialized), nameof(OnInitialize)));
+
+			List<T> result = !_data.TryGetValue(typeof(T), out var map) ? new List<T>() : new List<T>(map.Count);
+
+			if (map == null)
+				return result;
+
+			foreach (GameDataSO value in map.Values)
+				result.Add(UnityEngine.Object.Instantiate((T)value));
+
+			return result;
+		}
+
+		/// <summary>
 		/// Gets an item of type <typeparamref name="T"/>.
 		/// </summary>
 		/// <remarks>
@@ -68,7 +116,7 @@ namespace Gytis0.SOManager.Runtime
 			where T : GameDataSO
 		{
 			if (!Initialized)
-				throw new Exception("GameData not initialized. Wait for it to initialize first.");
+				throw new Exception(string.Format("GameData not initialized. You can use a '{0}' flag and / or '{1}' event to wait for initialization.", nameof(Initialized), nameof(OnInitialize)));
 
 			if (_data.TryGetValue(typeof(T), out var map) && map.TryGetValue(Convert.ToInt32(id), out var value))
 				return (T)value;
